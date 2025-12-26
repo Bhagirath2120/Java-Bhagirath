b69d7bed09d8ada292090c06181b1ac7042241a9
Bhagirath2120 <bhagirathr2005@gami.com>
2025-12-26 17:05:22 +0530

    percentage and area of circle

---
+++ src/basics/Area_of_circle.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class Area_of_circle {
+    public static void main(String[] args) {
+        double r = 5;
+        double Pi = 3.1415;
+        double Area = Pi * r * r;
+        System.out.println("Area of circle is "+Area);
+
+    }
+}
---
+++ src/basics/calculate_percentage_of_5_subject.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class calculate_percentage_of_5_subject {
+    public static void main(String[] args) {
+        double math = 95;
+        double english = 98;
+        double hindi = 97;
+        double science = 65;
+        double socialScience = 78;
+
+        double P =( math + english + hindi + science + socialScience)/5;
+
+        System.out.println("percentage = "+P);
+
+
+    }
+}
