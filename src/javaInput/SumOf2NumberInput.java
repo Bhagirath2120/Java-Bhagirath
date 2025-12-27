963a6f7c50907ceadf8754b0f24984ce9dc442ba
Bhagirath2120 <bhagirathr2005@gami.com>
2025-12-27 16:28:18 +0530

    Sum of 2 Number Input

---
+++ src/javaInput/SumOf2NumberInput.java
@@ -0,0 +1,16 @@
+package javaInput;
+import java.util.Scanner;
+public class SumOf2NumberInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter 1st Number:");
+               double x = sc.nextDouble(); //  for input
+        System.out.print("Enter 2nd Number:");
+               double y = sc.nextDouble();
+
+               double sum = x+y;   //operation
+
+        System.out.print("Sum = "+sum);
+    }
+}
