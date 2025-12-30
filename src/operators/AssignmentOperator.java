ce81d6ba02bc714cec6037b97dd15b7815f96c81
Bhagirath Rai <bhagirathr2005@gmail.com>
2025-12-30 17:39:06 +0530

    assignment Operator

---
+++ src/operators/AssignmentOperator.java
@@ -0,0 +1,23 @@
+package operators;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int p = 10;
+        int q;
+
+        q = p; // q = 10;
+        System.out.println(q); //output 10
+
+        p += q;   // p = p + q; p = 10 + 10 = 20
+        System.out.println(p); //output 20
+
+         p -= q;  // p = p + q;  p = 20 - 10 = 10
+        System.out.println(p); //output 10
+
+         p *= q; // p = p * q; p = 10 * 10 = 100
+        System.out.println(p); //output 100
+
+        p /= q; // p = p/q; p = p/q = 10
+        System.out.println(p); //output 10
+    }
+}
