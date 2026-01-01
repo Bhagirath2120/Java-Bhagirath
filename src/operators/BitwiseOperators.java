845e557a9e226479e36559c18ba95f7ba7a4372e
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-01 22:50:12 +0530

    even odd

---
+++ src/operators/BitwiseOperators.java
@@ -0,0 +1,17 @@
+package operators;
+
+public class BitwiseOperators {
+    public static void main(String[] args) {
+         int a = 9, b = 10;
+        System.out.println( a | b);      // output 11
+        System.out.println( a & b);     //output 8
+        System.out.println( a ^ b);    //output  3
+        // Left shift
+        System.out.println( a << 1);    //  output 18   a * b^2 = a<<1 =    a * 1^2
+        System.out.println( a << 2);   //  output 36    a * b^2 = a<<2
+
+        // right shift
+        System.out.println( b >> 1);    //output 5
+        System.out.println( b >> 2);   //output  2
+    }
+}
