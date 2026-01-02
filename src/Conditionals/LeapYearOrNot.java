4be8a88d0ebae592cef418c8024514b1868c0630
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-02 11:00:54 +0530

    Absolute value

--- src/Conditionals/LeapYearOrNot.java
+++ src/Conditionals/LeapYearOrNot.java
@@ -1,4 +1,18 @@
 package Conditionals;
 
+import java.util.Scanner;
+
 public class LeapYearOrNot {
+    public static void main(String[] args) {
+        System.out.print("Enter Year: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%4==0  && n%400==0){
+            System.out.print("Yes, This year is a Leap year");
+        }
+
+        else {
+            System.out.print("No, This year is not a Leap year");
+        }
+    }
 }
