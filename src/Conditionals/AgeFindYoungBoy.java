e5b07922872aef9747519911481a4c82e9849376
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-04 21:13:47 +0530

    find young man

---
+++ src/Conditionals/AgeFindYoungBoy.java
@@ -0,0 +1,27 @@
+package Conditionals;
+import java.util.Scanner;
+public class AgeFindYoungBoy {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ram Enter your age : ");
+        int A = sc.nextInt();
+        System.out.print("Shyam Enter your age : ");
+        int B = sc.nextInt();
+        System.out.print("Ajay Enter your age : ");
+        int C = sc.nextInt();
+
+
+        if(A<B && A<C){
+            System.out.println("Ram is young ");
+        }
+        else if(B<A && B<C){
+            System.out.println("Shyam is young ");
+        }
+        else {
+            System.out.println("Ajay is young ");
+        }
+
+    }
+}
