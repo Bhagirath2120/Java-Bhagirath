f4bb930055a9641baf661308a5d83550998090db
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-04 21:12:11 +0530

    Triangle

---
+++ src/Conditionals/SidesOfATriangle.java
@@ -0,0 +1,23 @@
+package Conditionals;
+import java.util.Scanner;
+
+public class SidesOfATriangle {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first side: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second side: ");
+        int b = sc.nextInt();
+        System.out.print("Enter third side: ");
+        int c = sc.nextInt();
+
+        if(a+b>c && a+c>b && b+c>a){
+            System.out.println("Valid triangle");
+        }
+        else{
+            System.out.println("Invalid triangle");
+        }
+    }
+}
