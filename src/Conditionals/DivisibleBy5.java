ad8acff2f473c01576eb0b24230cc0f14788cd11
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-08 22:55:55 +0530

    DivisibleBy5

--- src/Conditionals/DivisibleBy5.java
+++ src/Conditionals/DivisibleBy5.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class DivisibleBy5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Hello my name is Bhagirath Rai");
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
         if(n%5==0){
