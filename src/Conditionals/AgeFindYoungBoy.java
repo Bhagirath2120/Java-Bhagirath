a77b9a31e93a48dad0ab715118daaad620af869b
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-08 22:52:27 +0530

    AgeFindYoungBoy

--- src/Conditionals/AgeFindYoungBoy.java
+++ src/Conditionals/AgeFindYoungBoy.java
@@ -4,7 +4,7 @@ public class AgeFindYoungBoy {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("Hello my name is Bhagirath Rai");
         System.out.print("Ram Enter your age : ");
         int A = sc.nextInt();
         System.out.print("Shyam Enter your age : ");
