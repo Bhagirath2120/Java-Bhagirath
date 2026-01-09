a3d85a7714deee885a97177050004ac4a9ecde14
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-09 17:52:14 +0530

    for loop reverse N number

---
+++ src/Loops/PrintReverseN_number.java
@@ -0,0 +1,17 @@
+package Loops;
+import java.util.Scanner;
+public class PrintReverseN_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.println("Reverse Order");
+
+
+        for (int i = n; i>0; i--){
+
+            System.out.println(i);
+
+        }
+    }
+}
