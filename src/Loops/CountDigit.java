e7808600a6c7feb46acf22511bbd7a54f7ccccf2
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-10 10:14:39 +0530

    count digits

---
+++ src/Loops/CountDigit.java
@@ -0,0 +1,35 @@
+package Loops;
+import java.util.Scanner;
+
+// count digits in number
+public class CountDigit {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int n = sc.nextInt();
+
+
+        int count =0;
+
+
+        while(n>0){
+            n =n/10;
+            count++;
+        }
+        System.out.println("Total digits " +count);
+
+
+        //==================================================================================================
+
+        //for loop
+
+//       for(; n !=0; n /=10){
+//           count++;
+//       }
+//        System.out.println("Total digits " +count);
+
+
+    }
+}
