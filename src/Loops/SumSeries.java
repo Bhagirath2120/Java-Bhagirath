f86f8bc4a595e82f3c6cf46c91e77ee0c4c7c5ff
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-11 16:40:22 +0530

    Sum series

---
+++ src/Loops/SumSeries.java
@@ -0,0 +1,40 @@
+package Loops;
+
+import java.util.Scanner;
+// find the sum of following series, S = 1-2+3-4+5......n
+
+public class SumSeries {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number :");
+        int n = sc.nextInt();
+        int ans = 0;
+
+
+//         FOR LOOP
+
+//        for( int i = 1; i <= n; i++){
+//            if (i % 2 == 0) {
+//                ans -= i;
+//            }else{
+//                ans += i;
+//            }
+//        }
+
+        // while loop
+
+        int w = 1;
+
+        while(n>=w){
+            if(n%2==0){
+                ans-=n;
+            }
+            else{
+                ans+=n;
+            }
+                n--;
+        }
+
+        System.out.println("Sum Series : "+ans);
+    }
+}
