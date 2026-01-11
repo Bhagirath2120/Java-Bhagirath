1c44a7942fdd30e78f244d29ad89cb9aa6e290d4
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-11 16:41:15 +0530

    Reverse input digits

---
+++ src/Loops/ReverseInputDigits.java
@@ -0,0 +1,27 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class ReverseInputDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int n = sc.nextInt();
+        int rev = 0;
+
+//        while (n > 0) {
+//
+//            rev = (rev * 10) + (n % 10);
+//            n/=10;
+//
+//        }
+
+        for(; n>0; n/=10){
+            rev = (rev * 10) + (n % 10);
+
+        }
+        System.out.println("Reverse number : " + rev);
+
+    }
+}
+
