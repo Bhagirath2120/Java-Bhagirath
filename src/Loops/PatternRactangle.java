588c46a0b602cbd6113b1b18b49dbbf02cc2a95c
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-12 20:33:20 +0530

    Rectangle Pattern

---
+++ src/Loops/PatternRactangle.java
@@ -0,0 +1,37 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class PatternRactangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int r = sc.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int c = sc.nextInt();
+
+        // rectangle
+//        for(int i = 1; i <= r; i++) {
+//            for(int j = 1; j <= c; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        //empty rectangle
+
+        for(int i = 1; i <= r; i++){
+            for(int j = 1; j<=c; j++){
+
+                if( i == 1 || i ==  r || j == 1 || j == c){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
