0eee9366827cd00a5baaabe947e63f59d9194e14
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-13 20:48:55 +0530

    numerical pattern

---
+++ src/Loops/NumericalPattern.java
@@ -0,0 +1,24 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class NumericalPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int r = sc.nextInt();
+
+        for (int i = 1; i <= r; i++){
+
+            for (int j = i; j <= r; j++){
+                System.out.print(j);
+            }
+            for (int k = 1; k <= i-1; k++){
+                System.out.print(k);
+
+            }
+            System.out.println();
+        }
+
+    }
+}
