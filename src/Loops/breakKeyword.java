65f711c55b2eb85a5cf95cd430de7bb472717029
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-14 23:19:42 +0530

    Break keyword

--- src/Loops/breakKeyword.java
+++ src/Loops/breakKeyword.java
@@ -5,12 +5,14 @@ public class breakKeyword {
 
         int num = 1;
 
+        int count =0;
         while(true){
             if((num%5 == 0) && (num%7 == 0)){
                 System.out.println("Found answer "+num);
                 break;           // when answer  is found and at the time loop breaks
             }
             num++;
+            count++;
         }
     }
 }
