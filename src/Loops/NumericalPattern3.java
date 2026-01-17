dbdf16b8338aef252f678f514e970ac3721e61bc
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-17 18:29:11 +0530

    Pattern3

---
+++ src/Loops/NumericalPattern3.java
@@ -0,0 +1,23 @@
+package Loops;
+import java.util.Scanner;
+public class NumericalPattern3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter row : ");
+        int r = sc.nextInt();
+        System.out.println("Enter column : ");
+        int c = sc.nextInt();
+        for (int i = 0; i <= r; i++) {
+            for (int j = 0; j <= c; j++) {
+                if((i+j)%2==0){
+                    System.out.print("1");
+                }else {
+                    System.out.print("2");
+                }
+
+            }
+            System.out.println();
+        }
+
+    }
+}
