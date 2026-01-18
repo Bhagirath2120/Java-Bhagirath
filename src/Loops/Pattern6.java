30af6e564027272fd0c560c5c2c34362f6acf92e
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-18 20:15:56 +0530

    Pattern

---
+++ src/Loops/Pattern6.java
@@ -0,0 +1,35 @@
+package Loops;
+import java.util.Scanner;
+public class Pattern6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter * value: ");
+        int r = sc.nextInt();
+
+
+            for (int i = 1; i <= r; i++) {
+
+                for (int j = 1; j <= r - i; j++) {
+                    System.out.print(" ");
+                }
+                for (int k = 1; k <= i; k++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+
+            }
+
+            for (int i = 1; i <= r - 1; i++) {
+
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 1; j <= r - i; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+
+            }
+
+    }
+}
