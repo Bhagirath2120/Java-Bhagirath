447cd4ea8f92fb109f74b88576e58f247c80fa16
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-18 20:14:31 +0530

    update Numerical pattern

--- src/Loops/NumericalPattern4.java
+++ src/Loops/NumericalPattern4.java
@@ -5,8 +5,7 @@ public class NumericalPattern4 {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter row: ");
         int r =sc.nextInt();
-        System.out.println("Enter column: ");
-        int c =sc.nextInt();
+
 
         for(int i=1;i<=r;i++){
             for(int j=1;j<=i;j++){
