100c85bbead9c7dc8bda96151402977e68a428d6
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-01-30 18:55:37 +0530

    add two number using class

---
+++ src/OOPs/AddTwoNUmber.java
@@ -0,0 +1,32 @@
+package OOPs;
+import java.util.Scanner;
+
+// new class
+class algebra{
+    public int Add(int a,int b){
+
+        int Z = a + b;
+
+        return Z;
+    }
+}
+
+
+
+// main function class
+public class AddTwoNUmber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        algebra a1 = new algebra();     //object created
+
+        System.out.println("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter second number: ");
+        int b = sc.nextInt();
+         int ans = a1.Add(a,b);
+        System.out.println("sum = "+ ans);
+
+
+    }
+}
