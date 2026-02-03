870e0ccc06379fc1b616d73a79eae1e556480a11
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-02-03 21:57:22 +0530

    area of circle 03/02/2026

--- src/basics/Area_of_circle.java
+++ src/basics/Area_of_circle.java
@@ -2,6 +2,7 @@ package basics;
 
 public class Area_of_circle {
     public static void main(String[] args) {
+
         double r = 5;
         double Pi = 3.1415;
         double Area = Pi * r * r;
