7710239f5b632112841f807c3e139f6a3ff5eaef
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-02-13 14:17:09 +0530

    decimal to binary

--- src/Loops/DecimalToBinary.java
+++ src/Loops/DecimalToBinary.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 // decimal to binary conversion
 
-public class DecimalToBinary {
+public class  DecimalToBinary {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter decimal number: ");
