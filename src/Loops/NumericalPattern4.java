f4284bceb809c544ae816c074c6c77884d2c8892
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-02-14 18:10:43 +0530

    pattern 4

--- src/Loops/NumericalPattern4.java
+++ src/Loops/NumericalPattern4.java
@@ -7,7 +7,7 @@ public class NumericalPattern4 {
         int r =sc.nextInt();
 
 
-        for(int i=1;i<=r;i++){
+         for(int i=1;i<=r;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(j);
             }
