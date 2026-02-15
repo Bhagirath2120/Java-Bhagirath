4c28778dbd4ccdb4c92ad13848255ee512e1fa88
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-02-15 22:14:55 +0530

    pattern 6

--- src/Loops/PatternRactangle.java
+++ src/Loops/PatternRactangle.java
@@ -8,7 +8,7 @@ public class PatternRactangle {
 
         System.out.print("Enter the number of rows: ");
         int r = sc.nextInt();
-        System.out.print("Enter the number of columns: ");
+        System.out.print("Enter  the number of columns: ");
         int c = sc.nextInt();
 
         // rectangle
