93d057c49c3d2e216a1d5b5bcece2c20fe89b151
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-02-17 21:49:26 +0530

    reverse n number

--- src/Loops/PrintReverseN_number.java
+++ src/Loops/PrintReverseN_number.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class PrintReverseN_number {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print(" Enter a number: ");
         int n = sc.nextInt();
         System.out.println("Reverse Order");
 
