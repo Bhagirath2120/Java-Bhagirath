6f71d72fac2a4015d06712ec7b1d0f92f586ac66
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-02-18 23:44:06 +0530

    triangular pattern 2

--- src/Loops/TriangularPattern2.java
+++ src/Loops/TriangularPattern2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class TriangularPattern2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of rows");
+         System.out.println("Enter the number of rows");
         int r = sc.nextInt();
 
         int count = 0;
