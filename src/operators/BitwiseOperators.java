69cdfbf436bfe75c6a143b52a8e4d29b38e19bb2
Bhagirath Rai <bhagirathr2005@gmail.com>
2026-02-22 14:58:20 +0530

    bit wise operator

--- src/operators/BitwiseOperators.java
+++ src/operators/BitwiseOperators.java
@@ -4,7 +4,7 @@ public class BitwiseOperators {
     public static void main(String[] args) {
          int a = 9, b = 10;
         System.out.println( a | b);      // output 11
-        System.out.println( a & b);     //output 8
+         System.out.println( a & b);     //output 8
         System.out.println( a ^ b);    //output  3
         // Left shift
         System.out.println( a << 1);    //  output 18   a * b^2 = a<<1 =    a * 1^2
